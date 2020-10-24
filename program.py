0b01652e1528b94bfb2effd27039a6405a02554f
nkhorvash <nkhorvash@gmail.com>
2020-10-23 21:25:25 -0700

    This is the Wizard Battle app as written in the class

---
+++ program.py
@@ -0,0 +1,66 @@
+import time
+
+from actors import Wizard, Creature, SmallAnimal, Dragon
+import random
+
+
+def main():
+    print_header()
+    game_loop()
+
+def print_header():
+    print('-----------------------------------')
+    print('         Wizard Battle')
+    print('-----------------------------------')
+    print()
+
+
+def game_loop():
+
+    creatures = [
+        SmallAnimal('Toad', 1),
+        Creature('Tiger', 13),
+        SmallAnimal('Bat', 3),
+        Dragon('Dragon', 50, 75, True),
+        Wizard('Evil Wizard', 1000)
+    ]
+
+    hero = Wizard('Gandolf', 75)
+
+
+    while True:
+
+        active_creature = random.choice(creatures)
+        print('A {} of level {} has appeared from a dark and foggy forest...'.format(active_creature.name, active_creature.level))
+        print()
+
+        cmd = input('Do you [a]ttack, [r]un away, or [l]ook around? ')
+        if cmd == 'a':
+            if hero.attack(active_creature):
+                creatures.remove(active_creature)
+            else:
+                print('The wizard runs and hides taking time to recover...')
+                print()
+                time.sleep(5)
+                print('The wizard returns revitalized!')
+        elif cmd == 'r':
+            print('The wizard has become unsure of his power and flees!!!')
+        elif cmd == 'l':
+            print('The wizard {} takes in the surroundings and sees:'.format(hero.name))
+            for c in creatures:
+                print(' * A {} of level {}'.format(c.name, c.level))
+        else:
+            print('OK, exiting game... bye!')
+            break
+
+
+        if not creatures:
+            print("You've defeated all the creatures, well done!")
+            break
+
+        print()
+        print()
+
+
+if __name__ == '__main__':
+    main()
